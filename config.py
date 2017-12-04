d35707801026dc0b02359acf0f0f34ae3a2a69d8
dangerousor <1192350292@qq.com>
2017-12-04 13:13:27 +0800

    file-upload change mysql user

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:liuxiaofeng@106.14.133.20:3306/SparkWebProject'
+SQLALCHEMY_DATABASE_URI = 'mysql://dangerousor:liuxiaofeng@106.14.133.20:3306/SparkWebProject'
 UPLOAD_FOLDER = 'E:\\tmp'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
